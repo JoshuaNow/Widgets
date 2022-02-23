8378380e56ea1cd1a6b003b2e1eb4d5f43b6837f
Joshua Lopez <jrenelopez92@gmail.com>
2022-02-22 22:52:51 -0600

    JL: fixed delay in initial search

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,30 +2,46 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 function Search() {
-  const [term, setTerm] = useState("");
+  const [term, setTerm] = useState("programming");
   const [results, setResults] = useState([]);
 
   console.log(results);
-  // we can't make useEffect as an async function
-  //   method 1
+  // useEffect with clean up function
+  //   useEffect(() => {
+  //     console.log("I just rendered");
+
+  //     return () => {
+  //       console.log("clean up function");
+  //     };
+  //   }, [term]); // when am i going to rerender
+
+  //   we can't make useEffect as an async function
+  //     method 1
   useEffect(() => {
-    // const search = async () => {
-    //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
-    //     params: {
-    //       action: "query",
-    //       list: "search",
-    //       origin: "*",
-    //       format: "json",
-    //       srsearch: term,
-    //     },
-    //   });
-    //   setResults(data.query.search);
-    // };
-    // const timeoutId = setTimeout(() => {
-    //   if (term) {
-    //     search();
-    //   }
-    // }, 500);
+    const search = async () => {
+      const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+        params: {
+          action: "query",
+          list: "search",
+          origin: "*",
+          format: "json",
+          srsearch: term,
+        },
+      });
+      setResults(data.query.search);
+    };
+    const timeoutId = setTimeout(() => {
+      if (term) {
+        search();
+      }
+    }, 1000);
+    if (term && !results.length) {
+      search();
+    } else {
+      return () => {
+        clearTimeout(timeoutId);
+      };
+    }
   }, [term]);
 
   const renderedResults = results.map((result) => {
