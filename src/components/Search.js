961eaae0a610da42a42883a8ee0941fe9b6f8bf9
Joshua Lopez <jrenelopez92@gmail.com>
2022-02-22 22:30:25 -0600

    JL: setting up a timeout

---
+++ src/components/Search.js
@@ -0,0 +1,100 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Search() {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+
+  console.log(results);
+  // we can't make useEffect as an async function
+  //   method 1
+  useEffect(() => {
+    // const search = async () => {
+    //   const { data } = await axios.get("https://en.wikipedia.org/w/api.php", {
+    //     params: {
+    //       action: "query",
+    //       list: "search",
+    //       origin: "*",
+    //       format: "json",
+    //       srsearch: term,
+    //     },
+    //   });
+    //   setResults(data.query.search);
+    // };
+    // const timeoutId = setTimeout(() => {
+    //   if (term) {
+    //     search();
+    //   }
+    // }, 500);
+  }, [term]);
+
+  const renderedResults = results.map((result) => {
+    return (
+      <div className="item" key={result.pageid}>
+        <div className="right floated content">
+          <a
+            href={`https://en.wikipedia.org?curid=${result.pageid}`}
+            className="ui button"
+          >
+            go
+          </a>
+        </div>
+        <div className="content">
+          <div className="header">{result.title}</div>
+          {result.snippet}
+        </div>
+      </div>
+    );
+  });
+
+  // method 2
+  //   useEffect(() => {
+  //     (async () => {
+  //       await axios.get("adasd");
+  //     })();
+  //   }, [term]);
+  //   // method 3
+  //   useEffect(() => {
+  //     axios.get("url").then((response) => {
+  //       console.log(response.data);
+  //     });
+  //   }, [term]);
+
+  // empty [] render only initial render
+  // nothing render initial render and after every re-render
+  //[aChangingDatatype] run after initial render and if data has changed since last render
+  //   //useEffect
+  //   useEffect(() => {
+  //     console.log("I only run ONCE");
+  //   }, []);
+
+  //   //useEffect
+  //   useEffect(() => {
+  //     console.log("I run on render and when data changes");
+  //   }, [term, term2]);
+  //useEffect
+  //   useEffect(() => {
+  //     console.log(
+  //       "I run initially and after a rerender but I am almost never used",
+  //     );
+  //   });
+
+  return (
+    <div>
+      <div className="ui form">
+        <div className="field">
+          <label>Enter Search Term </label>
+          <input
+            type="text"
+            className="input"
+            value={term}
+            onChange={(e) => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+      <div className="ui celled list"> {renderedResults}</div>
+    </div>
+  );
+}
+
+export default Search;
